5fed1bf8159200f43ba79debf3f168d209c1ebf1
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-09-05 14:51:43 +0700

    feat: edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'loremflickr.com',
+        hostname: 'assets.example.com',
         port: '',
         pathname: '/**',
       },
