b14570aa5a75947d1f782a17b6b7f825558028ff
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-09-05 15:38:26 +0700

    feat: add images with next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
+
 const Homepage = () => {
   return (
     <main className="container mx-auto">
       <h1 className="text-lg font-semibold">This is homepage</h1>
+      <img src="/images/Chair.jpg" alt="Chair" />
+      <Image
+        src="/images/Chair.jpg"
+        alt="Chair"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+      />
     </main>
   );
 };
