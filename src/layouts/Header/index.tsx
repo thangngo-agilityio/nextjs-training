5c00468d9581f96a8ef405b2196172bf84e4e379
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-09-18 18:32:24 +0700

    feat: edit header layout

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -13,50 +13,54 @@ import { ArrowIcon, CartIcon, HeartIcon, LogoIcon, SearchIcon } from '@/icons';
 import { ROUTER } from '@/constants';
 
 const Header = () => (
-  <Stack
-    px="52px"
-    pt="40px"
-    pb="24px"
-    flexDirection="row"
-    alignItems="center"
-    justifyContent="space-between"
-  >
-    <Stack w="100%" flexDirection="row" alignItems="center">
-      <Box>
-        <Link href={ROUTER.HOME} title="Home">
-          <LogoIcon />
-        </Link>
-      </Box>
-      <Stack flexDirection="row" alignItems="center">
-        <Flex ml="36px" alignItems="center" gap={1}>
-          <Text size="lg">Space Builder</Text>
-          <Text variant="tertiary">(Coming soon)</Text>
-          <ArrowIcon />
-        </Flex>
-        <Flex ml="40px" alignItems="center" gap={1}>
-          <Text size="lg">Products</Text>
-          <ArrowIcon />
-        </Flex>
+  <Flex justifyContent="center">
+    <Flex
+      w="100%"
+      maxW="1512px"
+      px="52px"
+      pt="40px"
+      pb="24px"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Stack w="100%" flexDirection="row" alignItems="center">
+        <Box>
+          <Link href={ROUTER.HOME} title="Home">
+            <LogoIcon />
+          </Link>
+        </Box>
+        <Stack flexDirection="row" alignItems="center">
+          <Flex ml="36px" alignItems="center" gap={1}>
+            <Text size="lg">Space Builder</Text>
+            <Text variant="tertiary">(Coming soon)</Text>
+            <ArrowIcon />
+          </Flex>
+          <Flex ml="40px" alignItems="center" gap={1}>
+            <Text size="lg">Products</Text>
+            <ArrowIcon />
+          </Flex>
+        </Stack>
+        <Box ml="22px" width="30%">
+          <InputField
+            placeholder="Search for minimalist chair"
+            onChange={() => {}}
+            variant="search"
+            leftIcon={<SearchIcon />}
+            rightIcon={
+              <CloseIcon as="button" color="text.200" onClick={() => {}} />
+            }
+            background="background.100"
+          />
+        </Box>
       </Stack>
-      <Box ml="22px" width="30%">
-        <InputField
-          placeholder="Search for minimalist chair"
-          onChange={() => {}}
-          variant="search"
-          leftIcon={<SearchIcon />}
-          rightIcon={
-            <CloseIcon as="button" color="text.200" onClick={() => {}} />
-          }
-          background="background.100"
-        />
-      </Box>
-    </Stack>
-    <Stack flexDirection="row" alignItems="center" gap="32px">
-      <HeartIcon />
-      <CartIcon />
-      <Avatar />
-    </Stack>
-  </Stack>
+      <Stack flexDirection="row" alignItems="center" gap="32px">
+        <HeartIcon />
+        <CartIcon />
+        <Avatar />
+      </Stack>
+    </Flex>
+  </Flex>
 );
 
 export default Header;
