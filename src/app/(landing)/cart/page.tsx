3ad0b98381004aae6b7eb0bb263c648312b196db
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-10-09 15:44:06 +0700

    feat: add metadaa, sitemap and robots file

--- src/app/(landing)/cart/page.tsx
+++ src/app/(landing)/cart/page.tsx
@@ -3,6 +3,13 @@ import { getCartItems } from '@/apis';
 
 // Components
 import { CartPage } from '@/ui';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Cart',
+  description:
+    'This is the Cart page in a comprehensive e-commerce web application designed to facilitate online shopping.',
+};
 
 const Cart = async () => {
   const { data: cartList } = await getCartItems();
