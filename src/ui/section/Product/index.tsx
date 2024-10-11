4644a50799a80dfcdf4cf31737438b4dffe343a0
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-10-11 11:15:35 +0700

    feat: update use client for form login and trending section

--- src/ui/section/Product/index.tsx
+++ src/ui/section/Product/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import { Suspense } from 'react';
 import {
--- src/ui/section/Trending/index.tsx
+++ src/ui/section/Trending/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Fragment, Suspense } from 'react';
 import {
   Box,
